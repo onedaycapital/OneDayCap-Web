a187d45e56f63cae7ef4e5a178cd1040761504df
onedaycapital <subs@onedaycap.com>
2026-01-30 12:40:00 -0600

    Add Amplitude analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200..800&display=swap"
           rel="stylesheet"
         />
+        <script src="https://cdn.amplitude.com/libs/analytics-browser-2.11.1-min.js.gz"></script>
+        <script src="https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.25.0-min.js.gz"></script>
+        <script dangerouslySetInnerHTML={{__html: `window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));window.amplitude.init('17dea7f302865cf69e3fee139aa1885c', {"autocapture":{"elementInteractions":true}});`}} />
       </head>
       <body className="antialiased font-sans">{children}</body>
     </html>
